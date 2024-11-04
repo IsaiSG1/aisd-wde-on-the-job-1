2f3e38143bfc20f0290ac01a7e95583c9a4a5793
IsaiSG1 <isaisilva11@gmail.com>
2024-11-04 00:23:23 -0800

    Added foundations JS file

---
+++ index.js
@@ -0,0 +1,34 @@
+//Store Name: Pure Elegance
+const storeName = "Pure Elegance";
+
+//Store Type
+const storeType = "Beauty Salon";
+
+//services offered
+const service1= "Facial";
+const service2 = "Full Body Waxing";
+const service3 = "Lash Extensions";
+const service4 = "Tooth Gem";
+const service5 = "Microblading";
+const service6 = "Body Contouring";
+
+//service prices
+const service1Price = 50;
+const service2Price = 70;  
+const service3Price = 100;
+const service4Price = 25;
+const service5Price = 150;
+const service6Price = 200;
+
+//Store Hours
+const storeHours = "Monday/Wednesday/Thursday/Friday (10am - 7pm), Tuesday (Closed), Saturday (10am - 4pm), Sunday (Closed)"; 
+
+//Output: Console log all the variables
+console.log(storeName, storeType);
+console.log(service1, service1Price);
+console.log(service2, service2Price);
+console.log(service3, service3Price);
+console.log(service4, service4Price);
+console.log(service5, service5Price);
+console.log(service6, service6Price);
+console.log(storeHours);
