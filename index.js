617e2a74b7b0a089987d0b010259ca292cb0ad71
IsaiSG1 <isaisilva11@gmail.com>
2024-12-03 21:40:46 -0800

    Finalized OTJ3

--- index.js
+++ index.js
@@ -1,65 +1,74 @@
-//Store Name: Pure Elegance
+// Store Name: Pure Elegance
 const storeName = "Pure Elegance";
 
-//Store Type
+// Store Type
 const storeType = "Beauty Salon";
 
-//services offered
-const service1= "Facial";
-const service2 = "Full Body Waxing";
-const service3 = "Lash Extensions";
-const service4 = "Tooth Gem";
-const service5 = "Microblading";
-const service6 = "Body Contouring";
+// Services Offered and Prices
+const services = ["Facial", "Full Body Waxing", "Lash Extensions", "Tooth Gem", "Microblading", "Body Contouring"];
+const prices = [50, 70, 100, 25, 150, 200];
 
-//service prices
-const service1Price = 50;
-const service2Price = 70;  
-const service3Price = 100;
-const service4Price = 25;
-const service5Price = 150;
-const service6Price = 200;
+// Store Hours
+const storeHours = "Monday/Wednesday/Thursday/Friday (10am - 7pm), Tuesday (Closed), Saturday (10am - 4pm), Sunday (Closed)";
 
-//Store Hours
-const storeHours = "Monday/Wednesday/Thursday/Friday (10am - 7pm), Tuesday (Closed), Saturday (10am - 4pm), Sunday (Closed)"; 
+// Reusable logging function
+const logMessage = (message) => {
+  console.log(message);
+};
 
-/*/Output: Console log all the variables
-console.log(storeName, storeType);
-console.log(service1, service1Price);
-console.log(service2, service2Price);
-console.log(service3, service3Price);
-console.log(service4, service4Price);
-console.log(service5, service5Price);
-console.log(service6, service6Price);
-console.log(storeHours);*/
+// Function to display store info
+const displayStoreInfo = (storeName, storeType) => {
+  logMessage(`Welcome to ${storeName}! We are the best ${storeType} in town.`);
+};
 
-console.log("Welcome to ${storeName}! We are the best ${storeType} in town");
-console.log("Here are the services we currently offer:");
-let services = [service1, service2, service3, service4, service5, service6];
-let prices = [service1Price, service3Price, service3Price, service4Price, service5Price, service6Price];
-console.log("Services:", services);
-console.log("Prices:", prices);
-// added push new service and price to the arrays
-services.push("Manicure");
-prices.push(25);
-console.log("We've added a New Service:", services);
-console.log("Updated Prices:", prices);
-//removed the last service and price from the arrays
-services.pop();
-prices.pop();
-console.log("Unofortunately we no longer offer Microblading. Here are our updated services and prices:", services);
-console.log("Updated Prices:", prices);
-//created a new array with the first three services, selected featured services from the array
-let featuredServices = services.slice(0, 3);
-console.log("This week our Featured Services are:", featuredServices);
-//added new services and prices to the stores services and prices
-let newServices = ["Haircut", "Massage"];
-let newPrices = [30, 50];
-services = services.concat(newServices);
-prices = prices.concat(newPrices);
+// Function to display services and prices
+const displayInventory = (services, prices) => {
+  logMessage("Here are the services we currently offer:");
+  services.forEach((service, index) => {
+    logMessage(`${service}: $${prices[index]}`);
+  });
+};
 
-console.log("We are excited to announce that we now offer Haircuts and Massages! Here are our updated services", services);
-console.log("Updated Prices", prices);
+// Function to add a service
+const addService = (services, prices, newService, newPrice) => {
+  services.push(newService);
+  prices.push(newPrice);
+  logMessage(`We've added a new service: ${newService} for $${newPrice}`);
+  displayInventory(services, prices);
+};
 
-console.log("Thanks for visiting ${storeName}! We hope to see you soon!");
-console.log("Our hours are for reference:", storeHours);
+// Function to remove the last service
+const removeLastService = (services, prices) => {
+  const removedService = services.pop();
+  const removedPrice = prices.pop();
+  logMessage(`Unfortunately, ${removedService} is no longer offered.`);
+  displayInventory(services, prices);
+};
+
+// Function to display featured services (sorted alphabetically)
+const displayFeaturedServices = (services, numberOfFeatured) => {
+  const sortedServices = [...services].sort(); // Sort alphabetically
+  const featuredServices = sortedServices.slice(0, numberOfFeatured);
+  logMessage("This week, our featured services are:");
+  featuredServices.forEach((service) => logMessage(service));
+};
+
+// Function to sort services by price (low to high)
+const sortInventoryByPrice = (services, prices) => {
+  const sortedPairs = prices
+    .map((price, i) => [i, price]) // Create pairs of [index, price]
+    .sort((a, b) => a[1] - b[1]); // Sort pairs by price
+
+  logMessage("Services sorted by price (low to high):");
+  sortedPairs.forEach(([i]) => {
+    logMessage(`${services[i]}: $${prices[i]}`);
+  });
+};
+
+// Test the functions
+displayStoreInfo(storeName, storeType);
+displayInventory(services, prices);
+addService(services, prices, "Hair Styling", 80);
+removeLastService(services, prices);
+displayFeaturedServices(services, 3);
+sortInventoryByPrice(services, prices);
